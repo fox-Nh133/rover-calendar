0186e8f862a85f8b37f267f6445d90a9ee0e6593
Masaya Mac <106661910+fox-Nh133@users.noreply.github.com>
2024-05-06 04:23:35 +0900

    Update event modal window styles and add event details to calendar events

--- script.js
+++ script.js
@@ -50,9 +50,22 @@ document.addEventListener('DOMContentLoaded', () => {
         locale: 'ja',
         events: fullCalendarEvents,
         eventClick: function(info) {
+          // set event title
           document.getElementById('eventTitle').textContent = info.event.title;
-          document.getElementById('eventStartDate').textContent = info.event.start.toLocaleString();
-          document.getElementById('eventEndDate').textContent = info.event.end.toLocaleString();
+          // set event date
+          function isSameDate(date1, date2) {
+            return date1.getFullYear() === date2.getFullYear() &&
+              date1.getMonth() === date2.getMonth() &&
+              date1.getDate() === date2.getDate();
+          }
+          document.getElementById('eventStartDate').textContent = info.event.start.toLocaleString('ja-JP', { weekday: 'short', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'});
+
+          if (isSameDate(info.event.start, info.event.end)) {
+            document.getElementById('eventEndDate').textContent = info.event.end.toLocaleString('ja-JP', { hour: 'numeric', minute: 'numeric'});
+          } else {
+            document.getElementById('eventEndDate').textContent = info.event.end.toLocaleString('ja-JP', { weekday: 'short', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'});
+          }
+          // set event location and description
           document.getElementById('eventLocation').textContent = info.event.extendedProps.location;
           document.getElementById('eventDescription').innerHTML = info.event.extendedProps.description;
           document.getElementById('eventDetails').style.display = 'block';
@@ -61,12 +74,14 @@ document.addEventListener('DOMContentLoaded', () => {
       calendar.render();
     }).catch(err => console.error(err));
 
-    var closeEventDetails = document.getElementById('closeEventDetails');
-    if (closeEventDetails) {
-      closeEventDetails.addEventListener('click', function() {
+    // close event details
+    const closeEventDetails = document.querySelectorAll('.close-event-details');
+
+    closeEventDetails.forEach(el => {
+      el.addEventListener('click', () => {
         document.getElementById('eventDetails').style.display = 'none';
       });
-    }
+    });
 
     // init current date
     var today = new Date();
@@ -80,6 +95,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('currentDate').textContent = formattedDate;
     document.getElementById('currentDay').textContent = formattedWeekday;
+
+    // init closest event window
+    //// get closest event
+    getEventData().then(events => {
+      const closestEvent = events.filter(event => event.start > today).sort((a, b) => a.start - b.start)[0];
+      return closestEvent;
+    }).then(closestEvent => {
+      if (closestEvent) {
+        const closestEventDate = closestEvent.start.toLocaleDateString('ja-JP', options);
+        const closestEventWeekday = closestEvent.start.toLocaleDateString('ja-JP', weekday);
+        document.getElementById('closestEventDate').textContent = closestEventDate;
+        document.getElementById('closestEventDay').textContent = closestEventWeekday;
+        document.getElementById('closestEventTitle').textContent = closestEvent.title;
+        document.getElementById('closestEventLocation').textContent = closestEvent.location;
+        document.getElementById('closestEventDescription').textContent = closestEvent.description;
+      } else {
+        document.getElementById('closestEventDate').textContent = 'No upcoming events';
+      }
+    }).catch(err => console.error(err));
  
 
   });
