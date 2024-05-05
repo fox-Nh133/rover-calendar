475710300fac6bed192afcc6c9ea6bc34bfacf57
Masaya Mac <106661910+fox-Nh133@users.noreply.github.com>
2024-05-06 00:38:06 +0900

    Update calendar events and fetch script

--- script.js
+++ script.js
@@ -23,31 +23,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // init monthly calendar (fullcalendar integration)
-    //// parse ics url
-    const calendarIcsUrl = 'https://calendar.google.com/calendar/ical/27368b164f2ff54d4b7f165793fba4d2ef0706b2de617768c8c030ad0500e14c%40group.calendar.google.com/public/basic.ics'
-    fetch(calendarIcsUrl)
-      .then(response => response.text())
-      .then(data => {
-        const jcalData = ICAL.parse(data);
-        const comp = new ICAL.Component(jcalData);
-        const mainEvents = comp.getAllSubcomponents('vevent').map(vevent => {
-            const event = new ICAL.Event(vevent);
-            return {
-                title: event.summary,
-                start: event.startDate.toJSDate(),
-                end: event.endDate.toJSDate()
-            };
-        });
+    //// parse json data
+    async function getEventData() {
+      const response = await fetch('backend/calendar.json');
+      const data = await response.json();
+      const events = data.map(event => {
+        return {
+          id: event.uid,
+          title: event.summary,
+          start: new Date(event.start),
+          end: new Date(event.end)
+        }
+      });
+      return events;
+    }
 
-        //// render calendar 
-        var mainCalendarEl = document.getElementById('calendar');
-        var mainCalendar = new FullCalendar.Calendar(mainCalendarEl, {
-            initialView: 'dayGridMonth',
-            events: mainEvents
-        });
-        mainCalendar.render();
-      })
-      .catch(error => console.error('Error fetching data: ', error));
+    getEventData().then(events => console.log(events)).catch(err => console.error(err));
+
+    //// init fullcalendar
+    getEventData().then(fullCalendarEvents => {
+      const calendarEl = document.getElementById('main-calendar');
+      const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'ja',
+        events: fullCalendarEvents,
+        eventContent: function(arg) {
+          return { html: `<div class="fc-daygrid-event-dot"></div> <div class="fc-event-title">${arg.event.title}</div>`};
+        }
+      });
+      calendar.render();
+    }).catch(err => console.error(err));
 
     // init current date
     var today = new Date();
@@ -61,40 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('current-date').textContent = formattedDate;
     document.getElementById('current-day').textContent = formattedWeekday;
-
-    // // init weekly calendar
-    // const isoStart = today.toISOString();
-    // const isoEnd = endDate.toISOString();
-
-    // //// API request
-    // const apiKey = 'AIzaSyCsHXchDeAV3aW3NU6XC69K3bnzX69iJDs';
-    // const calendarId = '27368b164f2ff54d4b7f165793fba4d2ef0706b2de617768c8c030ad0500e14c@group.calendar.google.com'
-    // const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&timeMin=${isoStart}&timeMax=${isoEnd}&singleEvents=true&orderBy=startTime`;
-
-    // fetch(url)
-    // .then(response => response.json())
-    // .then(data => {
-    //     renderEvents(data.items);
-    // })
-    // .catch(error => console.error('Error fetching data: ', error));
-
-    // //// render calendar
-    // function renderEvents(events) {
-    //   const listContainer = document.getElementById('eventList');
-    //   listContainer.innerHTML = '';  // 既存のリストをクリア
-  
-    //   events.forEach(event => {
-    //       const listItem = document.createElement('div');
-    //       const eventDate = new Date(event.start.date).toLocaleDateString('ja-JP');
-    //       const eventTime = `${new Date(event.start.date).toLocaleTimeString('ja-JP')} - ${new Date(event.end.date).toLocaleTimeString('ja-JP')}`;
-    //       listItem.innerHTML = {
-    //           title: event.summary,
-    //           date: eventDate,
-    //           time: eventTime
-    //       };
-    //       listContainer.appendChild(listItem);
-    //   });
-    // }
  
 
   });
