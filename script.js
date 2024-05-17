eeb08511ddb5276a7481621801ee3cdb02d4aee9
Masaya Mac <106661910+fox-Nh133@users.noreply.github.com>
2024-05-18 08:40:43 +0900

    add service worker

--- script.js
+++ script.js
@@ -21,6 +21,28 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
 
+    // Register service worker
+    async function registerServiceWorker() {
+      if ("serviceWorker" in navigator) {
+        try {
+          const registration = await navigator.serviceWorker.register("/service-worker.js", {
+            scope: "/",
+          });
+          if (registration.installing) {
+            console.log("Service worker installing");
+          } else if (registration.waiting) {
+            console.log("Service worker installed");
+          } else if (registration.active) {
+            console.log("Service worker active");
+          }
+        } catch (error) {
+          console.error(`Registration failed with ${error}`);
+        }
+      }
+    }
+
+    registerServiceWorker();
+
     // init add event buttons
     const addEventButton = document.getElementById('addEventButton');
 
